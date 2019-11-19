da5c9a3944910b3787e3b662b9e773c593c26ff2
Yuvaraju1729 <55615610+Yuvaraju1729@users.noreply.github.com>
2019-11-19 11:57:47 +0530

    Update deploy_performance.groovy

--- vars/deploy_performance.groovy
+++ vars/deploy_performance.groovy
@@ -1,5 +1,5 @@
 def call(String msg = 'deploy_per') {
 echo "${msg}"
-                 sh 'ssh -t -t -i /var/lib/jenkins/.ssh/id_rsa ansadmin@172.31.31.91 "ansible-playbook /opt/playbooks/performance.yml"'
+                 sh 'ssh -t -t -i /var/lib/jenkins/.ssh/id_rsa ansadmin@172.31.37.71 "ansible-playbook /opt/playbooks/performance.yml"'
 
 }
