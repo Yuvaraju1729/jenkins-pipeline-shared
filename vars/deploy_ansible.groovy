b6417f687f287dbb8297abe7ae16688abb61e331
Yuvaraju1729 <55615610+Yuvaraju1729@users.noreply.github.com>
2019-11-19 12:02:30 +0530

    Update deploy_ansible.groovy

--- vars/deploy_ansible.groovy
+++ vars/deploy_ansible.groovy
@@ -1,4 +1,4 @@
 def call(String msg = 'deploy_ansible') {
 echo "${msg}"
-  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.31.91:/projects'
+  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.37.71:/projects'
 }
